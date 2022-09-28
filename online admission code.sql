9a644a6795ae4f32e58fbd270f6fd7205c1ce093
Furkhan76 <98233878+Furkhan76@users.noreply.github.com>
2022-09-28 20:26:48 +0530

    Create online admission code.sql

---
+++ online admission code.sql
@@ -0,0 +1,22 @@
+
+  
+CREATE TABLE Admin (
+    aid int,
+    password varchar(255)
+);
+
+create table student(
+  sid int,
+  password varchar(255),
+  name varchar(255),
+  email id varchar(255),
+  contact no long int
+  );
+  
+  insert into Admin values(111,'ram');
+  insert into Admin values(112,'ramu');
+  insert into student values(001,'yy','joe','joe@gmail.com','9874562147');
+  insert into student values(002,'zz','jai','jai@gmail.com','8745125478');
+  
+  
+                        
